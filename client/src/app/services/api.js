e924c5bf73d692c17b29e82a72c8f018830f17e0
AARTHIBALAMURUGAN <aarthib875@gmail.com>
2026-05-23 22:04:56 +0530

    change backend url

--- client/src/app/services/api.js
+++ client/src/app/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expensesplitter-yj5b.onrender.com/",
+  baseURL: "https://expensesplitter-yj5b.onrender.com/api",
 });
 
 API.interceptors.request.use(
